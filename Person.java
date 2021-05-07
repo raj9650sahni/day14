3c19402318e457fea9c11faa574f72e0b89fa494
Raj Kishore Sahni <63728002+raj9650sahni@users.noreply.github.com>
2021-05-07 14:43:04 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,7 +1,7 @@
 package com.addressbok.model;
 
 public class Person {
-	 
+
 	public String name;
 	public String age;
 	public String email;
@@ -10,35 +10,95 @@ public class Person {
 	public String zipcode;
 	public String state;
 	public String city;
-	
-	
-	public Person(String name,String age,String email,String address,String phone,String zipcode,String state,String city) {
-		
-		this.name =  name;
+
+	public Person(String name, String age, String email, String address, String phone, String zipcode, String state,
+			String city) {
+
+		this.name = name;
 		this.age = age;
 		this.email = email;
 		this.phone = phone;
-		this.address =address;
+		this.address = address;
 		this.zipcode = zipcode;
-		this.state= state;
+		this.state = state;
 		this.city = city;
-;		
-		
+		;
+
 	}
-	
+
+	public void setCity(String city) {
+		this.city = city;
+
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+
+	}
+
+	public void setState(String state) {
+		this.state = state;
+
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getZip() {
+		return zipcode;
+	}
+
 	public String toString() {
-		return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", address='" + address + '\'' +
-                ", zipcode='" + zipcode + '\'' +
-                ", state='" + state + '\'' +
-                ", city='" + city+ '\'' +
-                
-                '}';
+		return "Person{" + "name='" + name + '\'' + ", age=" + age + ", email='" + email + '\'' + ", phone='" + phone
+				+ '\'' + ", address='" + address + '\'' + ", zipcode='" + zipcode + '\'' + ", state='" + state + '\''
+				+ ", city='" + city + '\'' +
+
+				'}';
 	}
 
 }
-
