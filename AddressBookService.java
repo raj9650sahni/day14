665af51fcc8d0215f6f46f524673dbe19ac8f87a
Raj Kishore Sahni <63728002+raj9650sahni@users.noreply.github.com>
2021-05-07 14:42:08 +0530

    Update AddressBookService.java

--- AddressBookService.java
+++ AddressBookService.java
@@ -3,11 +3,23 @@ package com.bl.addressbook.service;
 import com.addressbok.model.Person;
 import com.bl.addressbook.service.IAddressBookService;
 
+package com.bl.addressbook.service;
+
+import com.addressbok.model.Person;
+import com.bl.addressbook.service.IAddressBookService;
+
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class AddressBookService implements IAddressBookService {
-	private List<Person>  addressBookList;
+	private List<Person> addressBookList;
 
 	public AddressBookService() {
 		addressBookList = new ArrayList<>();
@@ -33,6 +45,131 @@ public class AddressBookService implements IAddressBookService {
 	@Override
 	public List<Person> getAddressBook() {
 		return addressBookList;
+	}
+
+	public boolean editPerson(String contactName, Scanner scanner) {
+		for (int i = 0; i < addressBookList.size(); i++) {
+
+			if (addressBookList.get(i).name.equalsIgnoreCase(contactName))
+
+			{
+				System.out.println("Enter the FirstName");
+				String name = scanner.next();
+
+				System.out.println("Enter the Age");
+				String age = scanner.next();
+
+				System.out.println("Enter the Address");
+				String address = scanner.next();
+
+				System.out.println("Enter the City");
+				String city = scanner.next();
+
+				System.out.println("Enter the PinCode");
+				String pincode = scanner.next();
+
+				System.out.println("Enter the PhoneNo");
+				String phoneNo = scanner.next();
+
+				System.out.println("Enter the Email");
+				String email = scanner.next();
+
+				System.out.println("Enter the State");
+				String state = scanner.next();
+
+				addressBookList.get(i).setName(name);
+				addressBookList.get(i).setAge(age);
+				addressBookList.get(i).setCity(city);
+				addressBookList.get(i).setAddress(address);
+				addressBookList.get(i).setPhone(phoneNo);
+				addressBookList.get(i).setZipcode(pincode);
+				addressBookList.get(i).setEmail(email);
+				addressBookList.get(i).setState(state);
+				System.out.println("Updated list" + addressBookList.get(i));
+			}
+
+		}
+		return false;
+
+	}
+
+	public void hasPresent(String n) {
+
+		/*
+		 * for (int i = 0; i < addressBookList.size(); i++) { if
+		 * (addressBookList.get(i).name.equalsIgnoreCase(p)) { return true; } }
+		 * 
+		 * return false;
+		 */
+
+		List<Person> list2 = addressBookList.stream().filter(p -> p.getName().equalsIgnoreCase(n))
+				.collect(Collectors.toList());
+
+		if (list2.isEmpty()) {
+			System.out.println("person details not exist");
+		} else {
+			System.out.println("Person details exist");
+		}
+
+	}
+
+	@Override
+	public void showByState(String s) {
+		List<Person> list = addressBookList.stream().filter(p1 -> p1.getState().equalsIgnoreCase(s))
+				.collect(Collectors.toList());
+
+		System.out.println(list);
+
+	}
+
+	public void showByCity(String c) {
+		List<Person> list1 = addressBookList.stream().filter(p -> p.getCity().equalsIgnoreCase(c))
+				.collect(Collectors.toList());
+		System.out.println(list1);
+
+	}
+
+	public int count() {
+		int p = addressBookList.size();
+		return p;
+
+	}
+
+	public int countContactByState(String p) {
+		List<Person> list1 = addressBookList.stream().filter(c -> c.getState().equalsIgnoreCase(p))
+				.collect(Collectors.toList());
+
+		return list1.size();
+
+	}
+
+	public int countContactByCity(String p) {
+
+		List<Person> list1 = addressBookList.stream().filter(c -> c.getCity().equalsIgnoreCase(p))
+				.collect(Collectors.toList());
+
+		return list1.size();
+
+	}
+
+	public void sortByName() {
+		Collections.sort(addressBookList, Comparator.comparing(Person::getName));
+		System.out.println(addressBookList);
+	}
+
+	public void sortByState() {
+		Collections.sort(addressBookList, Comparator.comparing(Person::getState));
+		System.out.println(addressBookList);
+	}
+
+	public void sortByCity() {
+		Collections.sort(addressBookList, Comparator.comparing(Person::getCity));
+		System.out.println(addressBookList);
+	}
+
+	public void sortByZip() {
+		Collections.sort(addressBookList, Comparator.comparing(Person::getZip));
+		System.out.println(addressBookList);
 	}
 
 }
